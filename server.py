17daf3b01253e4a53a4da203ab68ddddc7d586d2
Lev <Levayu22@gmail.com>
2021-02-15 14:35:38 +0300

    add ConnectionError tracking
    Добавлено отслеживание ошибки подключения

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
 import requests
+from requests.exceptions import ConnectionError
 
 
 def get_storehouse_size():
-    response = requests.get(f'http://127.0.0.1:5000/scheme')
-
-    if response.status_code == 200:  # If request completed successfully
-        res = response.json()
+    try:
+        response = requests.get(f'http://127.0.0.1:5000/scheme')
+    except ConnectionError:
+        print('Connection Error')
     else:
-        res = None
-    return res
+        if response.status_code == 200:  # If request completed successfully
+            res = response.json()
+        else:
+            res = None
+        return res
 
 
 if __name__ == "__main__":
