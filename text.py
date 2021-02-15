fa5ddca7d6ca0f6e2cb3c2dc13b9c49d41acd9ed
Lev <Levayu22@gmail.com>
2021-02-15 12:57:56 +0300

    add main menu
    Добавлено главное меню из 5-ти кнопок

---
+++ text.py
@@ -0,0 +1,9 @@
+# File with phrases, witch used in interface
+
+main_menu = {
+    "init": "Initialize storehouse",
+    "add": "Add products to the storehouse",
+    "info": "Information about storehouse",
+    "remove": "Remove product from the storehouse",
+    "remote_info": "Information about remote storehouse"
+}
