4a4e218c1627edadb87fe6c0803cf5dff07e6e34
Lev Yuvenskiy <levayu22@gmail.com>
2021-02-15 19:11:37 +0300

    refactor main code

---
+++ gui/application.py
@@ -0,0 +1,28 @@
+# Main application's code
+
+import tkinter as tk
+
+import server.operations as server_operations
+from gui.widgets import MainMenu
+from gui.windowsParameters import WindowParams
+
+
+class MainWindow(tk.Tk):
+    """Application's main window"""
+
+    def __init__(self):
+        super().__init__()
+        self.parameters = WindowParams(title="Electronic storehouse", pady=300, padx=300)
+        self.title(self.parameters.title)
+        self.geometry(self.parameters.geometry())
+        self.resizable(*self.parameters.resizable)
+        try:
+            self.iconbitmap(self.parameters.ico_path)
+        except tk.TclError:  # Icon display error
+            pass  # Default Tkinter's icon
+        self.__menu = MainMenu(self, init_func=server_operations.init)
+
+    def run(self):
+        """Launching the app"""
+        self.__menu.draw()
+        self.mainloop()
