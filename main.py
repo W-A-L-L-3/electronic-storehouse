f77496e5dbbd87c15b6764aeb49f2736d7b03ad1
Lev <Levayu22@gmail.com>
2021-02-15 13:06:22 +0300

    edit paddings

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class MainWindow(tk.Tk):
 
     def __init__(self):
         super().__init__()
-        self.parameters = WindowParams(title="Electronic storehouse")
+        self.parameters = WindowParams(title="Electronic storehouse", pady=300, padx=300)
         self.title(self.parameters.title)
         self.geometry(self.parameters.geometry())
         self.resizable(*self.parameters.resizable)
