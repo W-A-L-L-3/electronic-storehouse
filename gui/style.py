f6875269bc6270c847422e2de587f5d2292c0210
Lev <Levayu22@gmail.com>
2021-02-17 22:19:42 +0300

    add adding-table
    Добавлена таблица с полями ввода для добавления товаров на склад

--- gui/style.py
+++ gui/style.py
@@ -6,8 +6,19 @@ font_name = "Helvetica"
 class Btn:
     """Style-settings for buttons"""
     font = (font_name, 11)
-    width = 28
+    big_width = 28
+    small_width = 10
     pady = 5
+
+
+class Entry:
+    """Style-settings for entries"""
+    font = (font_name, 11)
+    big_width = 18
+    small_width = 8
+    padx = 5
+    pady = 5
+
 
 class InfoTable:
     """Style-settings for table with info"""
