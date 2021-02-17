ac0994013ae2cea7ae88eb445fe5d0d7811df7f5
Lev Yuvenskiy <levayu22@gmail.com>
2021-02-17 22:29:36 +0300

    set timeout on 0.5s
    Установлен таймаут на 0,5сек (если "сервер" не получает ответ на запрос в течение этого времени), возвращается исключение

--- server/myRequests.py
+++ server/myRequests.py
@@ -1,7 +1,7 @@
 # File with my requests operations
 
 import requests
-from requests.exceptions import ConnectionError
+from requests.exceptions import ConnectionError, Timeout
 
 import constants as const
 import exceptions
@@ -10,9 +10,10 @@ import exceptions
 def get_parameters():
     """Get parameters of storehouse from API"""
     try:
-        response = requests.get(const.GET_PARAMS_ADDRESS)
-    except ConnectionError:
+        response = requests.get(const.GET_PARAMS_ADDRESS, timeout=0.5)
+    except (ConnectionError, Timeout):
         raise exceptions.ReceivingError
+
     else:
         if response.status_code != 200:  # If request is not completed successfully
             raise exceptions.ReceivingError
