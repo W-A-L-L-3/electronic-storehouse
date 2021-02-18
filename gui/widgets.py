64c6bdbea3245026cb499f581fd1a5535cc745cd
Lev Yuvenskiy <levayu22@gmail.com>
2021-02-18 11:16:53 +0300

    add checking correctness of the entered data

--- gui/widgets.py
+++ gui/widgets.py
@@ -191,16 +191,50 @@ class AddingWList(tk.Frame):
 
     def get_list(self):
         """Return list of items, info about witch in entries"""
-        list = []
+        items_list = []
         for row in self.__list:
-            name = row.get_name()
-            size = row.get_size()
-            mass = row.get_mass()
-            list.append(Item(name, size, mass, pos=None))
-        return list
+            name = row.formatted_name()
+            size = row.formatted_size()
+            mass = row.formatted_mass()
+            items_list.append(Item(name, size, mass, pos=None))
+        return items_list
 
 
 class AddingWRow:
+
+    @staticmethod
+    def __check_name(value):
+        """
+        Check value of field 'name'.
+        :return: True if it is correct, else False
+        """
+        return value != ""
+
+    @staticmethod
+    def __check_size(value):
+        """
+        Check value of field 'size'.
+        :return: True if it is correct, else False
+        """
+        seps = value.count("*") == 2
+        vals = False
+        if seps:
+            v = value.split("*")
+            vals = len(v) == 3 and v[0].isdigit() and v[1].isdigit() and v[2].isdigit()
+        return seps and vals
+
+    @staticmethod
+    def __check_mass(value):
+        """
+        Check value of field 'mass'.
+        :return: True if it is correct, else False
+        """
+        try:
+            a = float(value)
+        except ValueError:
+            return False
+        else:
+            return True
 
     def __init__(self, window, row_ind):
         self.__row_ind = row_ind
@@ -211,15 +245,19 @@ class AddingWRow:
 
         self.__name = tk.Entry(window,
                                font=style.Entry.font,
-                               width=style.Entry.big_width)
+                               width=style.Entry.name_f_width)
 
         self.__size = tk.Entry(window,
                                font=style.Entry.font,
-                               width=style.Entry.big_width)
+                               width=style.Entry.size_f_width)
 
         self.__mass = tk.Entry(window,
                                font=style.Entry.font,
-                               width=style.Entry.small_width)
+                               width=style.Entry.mass_f_width)
+
+        self.__name.bind("<Leave>", self.__color_name)
+        self.__size.bind("<Leave>", self.__color_size)
+        self.__mass.bind("<Leave>", self.__color_mass)
 
     def draw(self):
         self.__number.grid(row=self.__row_ind, column=0,
@@ -231,17 +269,84 @@ class AddingWRow:
         self.__mass.grid(row=self.__row_ind, column=3,
                          padx=style.Entry.padx, pady=style.Entry.pady)
 
-    def get_name(self):
+    def __get_name(self):
+        """Return str value of field 'name'"""
         value = str(self.__name.get())
         return value
 
-    def get_size(self):
+    def __get_size(self):
+        """Return str value of field 'size'"""
         value = str(self.__size.get())
         return value
 
-    def get_mass(self):
+    def __get_mass(self):
+        """Return str value of field 'mass'"""
         value = str(self.__mass.get())
         return value
+
+    def formatted_name(self):
+        """
+        :return: name in str if it is correct
+        """
+        value = self.__get_name()
+        if self.__check_name(value):
+            return value
+        else:
+            pass
+
+    def formatted_size(self):
+        """
+        :return: size in tuple(x, y, z) if it is correct
+        """
+        value = self.__get_size()
+        if self.__check_size(value):
+            t = value.split("*")
+            return tuple(t)
+        else:
+            pass
+
+    def formatted_mass(self):
+        """
+        :return: mass in int if it is correct
+        """
+        value = self.__get_mass()
+        if self.__check_mass(value):
+            return int(value)
+        else:
+            pass
+
+    def __color_name(self, event):
+        """
+
+        If it is correct, fill entry green, else red
+        """
+        value = self.__get_name()
+        if self.__check_name(value):
+            self.__name["bg"] = style.Entry.ok_color
+        else:
+            self.__name["bg"] = style.Entry.warning_color
+
+    def __color_size(self, event):
+        """
+        Check value in field 'size'.
+        If it is correct, fill entry green, else red
+        """
+        value = self.__get_size()
+        if self.__check_size(value):
+            self.__size["bg"] = style.Entry.ok_color
+        else:
+            self.__size["bg"] = style.Entry.error_color
+
+    def __color_mass(self, event):
+        """
+        Check value in field 'mass'.
+        If it is correct, fill entry green, else red
+        """
+        value = self.__get_mass()
+        if self.__check_mass(value):
+            self.__mass["bg"] = style.Entry.ok_color
+        else:
+            self.__mass["bg"] = style.Entry.error_color
 
 
 class AddingWButtons(tk.Frame):
